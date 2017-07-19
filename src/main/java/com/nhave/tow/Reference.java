67d28a5822cf895ac4e778e06d37a03492b4a8ab
nhave <nhave06@gmail.com>
2017-07-19 18:28:53 +0200

    LoL Forgot a file.

--- src/main/java/com/nhave/tow/Reference.java
+++ src/main/java/com/nhave/tow/Reference.java
@@ -3,11 +3,11 @@ package com.nhave.tow;
 public class Reference
 {
     public static final String FORGE_VERSION = "13.20.0.2253";
-    public static final String NHCORE_VERSION = "3.1.0";
+    public static final String NHCORE_VERSION = "3.2.0";
     
 	public static final String MODID = "tow";
     public static final String NAME = "The One Wrench";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.2.0";
     public static final String MCVERSIONS = "[1.11.2,)";
     public static final String DEPENDENCIES = 
     		"required-after:forge@[" + FORGE_VERSION + ",);"
